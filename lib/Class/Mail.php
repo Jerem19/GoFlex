833d20e0f7918002da8fb24a815ac0d519791911
jeremie.vianin <jeremie.vianin@hevs.ch>
2018-06-25 14:43:19 +0200

    test mail with explication 2

--- lib/Class/Mail.php
+++ lib/Class/Mail.php
@@ -13,7 +13,10 @@ class Mail {
         for ($i = 0; $i < $countLn; $i++) {
             $l10n = json_decode(file_get_contents(PUBLIC_FOLDER . 'l10n/' . L10NAvail[$i]["abr"] . '.json'), true)["mail"]["activation"];
 
-            $sub.= $l10n["subject"];
+            if($i == 0)
+            {
+                $sub.= $l10n["subject"];
+            }
             $text.= sprintf($l10n["text"], $user);
             $text .= "$newLine$newLine\t". Configuration::hostname . BASE_URL. "signup?id=" . $user->getToken();
             $text.= sprintf($l10n["textSec"], $user);
