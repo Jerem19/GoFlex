192f5ae56935071e28c2389258e69e25bca99591
TobiePraz <TobiPraz@edueptm.ch>
2019-02-14 11:14:00 +0100

    first 404

--- public/views/partials/index/404.php
+++ public/views/partials/index/404.php
@@ -1,2 +1,15 @@
-<?php
+<div class="form-panel flex-fill" id="notfound">
+    <h1 style="text-align: center;">404</h1>
+    <h3 style="text-align: center;"><?= L10N["index"]["404"]["main_text"] ?></h3>
 
+    <style>
+        #notfound {
+            animation: falling 5s linear 0s 1 normal ;
+        }
+
+        @keyframes falling {
+            0%   {transform: rotate(30deg); }
+            75%  {transform: translate(40px, 100px); }
+        }
+    </style>
+</div>
